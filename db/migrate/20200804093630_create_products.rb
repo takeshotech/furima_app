e05468726280f696341b3ce4bdd119457c61d927
mayukoala <mayu.3104.j@gmail.com>
2020-08-04 19:20:12 +0900

    Update 20200804093630_create_products.rb

--- db/migrate/20200804093630_create_products.rb
+++ db/migrate/20200804093630_create_products.rb
@@ -1,16 +1,16 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
+      t.references :category, null: false, foreign_key: true
+      t.references :shipping, null: false, foreign_key: true
+      t.references :brand, foreign_key:true
+      t.references :user, null: false, foreign_key: true
       t.string :name, null:false
       t.text :text, null:false
       t.integer :condition, null:false
       t.integer :price, null:false
       t.integer :trading_status, null:false
       t.datetime :completed_at
-      t.references :category, null: false, foreign_key: true
-      t.references :shipping, null: false, foreign_key: true
-      t.references :brand, foreign_key:true
-      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
