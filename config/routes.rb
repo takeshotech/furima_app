c25326670558e4bdd2cb252e7a2ab0e1e6f88ea6
mayukoala <mayu.3104.j@gmail.com>
2020-08-08 16:48:57 +0900

    FixScssFileName

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :products, only: [:index, :new]
+  resources :products, only: [:index, :new, :create]
 end
