d755bae2e0f5b59223041dd3e15e5bc5f8575bff
mayukoala <mayu.3104.j@gmail.com>
2020-08-08 16:30:50 +0900

    FixContorollerName
    productsコントローラーを新たに作成。
    それに伴い、viewファイル名やルーティングの内容も修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,5 +3,6 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @product = 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'items#index'
+  root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :items, only: [:index, :new]
+  resources :products, only: [:index, :new]
 end
