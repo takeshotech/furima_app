282dee30bb26ca048bb5f2cc46b1ecd1147146c9
Takeshotech <dancedancedance0014ex@gmail.com>
2020-08-15 00:48:28 +0900

    registration&login-server

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
-  
+  # before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   protected
 
   def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
+    authenticate_or_request_with_http_basic do |nickname, password|
       username == Rails.application.credentials[:basic_auth][:user] &&
       password == Rails.application.credentials[:basic_auth][:pass]
     end
@@ -13,4 +15,10 @@ class ApplicationController < ActionController::Base
   def production?
     Rails.env.production?
   end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:family_name,:first_name,:family_name_kana,:first_name_kana,:birth,:password,:email])
+
+  end
+
 end
--- db/migrate/20200805043522_add_devise_to_users.rb
+++ db/migrate/20200805043522_add_devise_to_users.rb
@@ -9,11 +9,10 @@ class AddDeviseToUsers < ActiveRecord::Migration[5.2]
       t.string :first_name,           null: false
       t.string :family_name_kana,       null: false
       t.string :first_name_kana,        null: false
-      t.string :birth_year,             null: false
-      t.string :birth_month,           null: false
-      t.string :birth_day,            null: false
+      t.string :birth,             null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :password_confirmation, null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
