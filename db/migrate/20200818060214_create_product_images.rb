3c646bb14e7418c1c57c30d7c563426a8339db4a
mayukoala <mayu.3104.j@gmail.com>
2020-08-18 15:55:34 +0900

    Update 20200818060214_create_product_images.rb

--- db/migrate/20200818060214_create_product_images.rb
+++ db/migrate/20200818060214_create_product_images.rb
@@ -1,8 +1,10 @@
 class CreateProductImages < ActiveRecord::Migration[5.2]
   def change
     create_table :product_images do |t|
-
+      t.string :image_url, null:false
+      t.references :product, foreign_key: true
       t.timestamps
+      
     end
   end
 end
