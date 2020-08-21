060b23139c715650ccd7b6b16ead1ffe80190313
mayukoala <mayu.3104.j@gmail.com>
2020-08-21 17:03:54 +0900

    AddColmun
    shippingsにtypeがなかったので追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_21_064155) do
+ActiveRecord::Schema.define(version: 2020_08_21_080145) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2020_08_21_064155) do
     t.integer "handing_time", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "type"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
