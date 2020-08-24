4c73ed7096baf484a10cc1204345cb4c174b6946
yumiki06 <65440153+yumiki06@users.noreply.github.com>
2020-08-25 00:44:31 +0900

    Add get_category_children action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,6 +6,11 @@ class ProductsController < ApplicationController
     # データベースから親カテゴリーのみ抽出し、配列化
     @category_parent = Category.where(ancestry: nil)
   end
+
+  # 親カテゴリーが選択された後に動くアクション
+  def get_category_children
+    @category_children = Category.find(params[:parent_id]).children
+  end
   def create
     @product = Product.create(product_params)
   end
