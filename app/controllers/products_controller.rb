0bdfc36cfcd0df32178a19cd8c79ad2768ae35fd
yumiki06 <65440153+yumiki06@users.noreply.github.com>
2020-08-25 00:44:44 +0900

    Add get_category_grandchildren action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,6 +11,12 @@ class ProductsController < ApplicationController
   def get_category_children
     @category_children = Category.find(params[:parent_id]).children
   end
+
+  # 子カテゴリーが選択された後に動くアクション
+  def get_category_grandchildren
+    @category_grandchildren = Category.find(params[:child_id]).children
+  end
+
   def create
     @product = Product.create(product_params)
   end
