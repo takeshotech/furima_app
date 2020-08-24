855546f72335835ff7f4b939c2812994792c578c
yumiki06 <65440153+yumiki06@users.noreply.github.com>
2020-08-25 00:45:54 +0900

    Add json settings for category function

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   root 'products#index'
   resources :products, only: [:index, :new, :create]
   resources :orders, only: :show
-
+  resources :products do
+    collection do
+      get 'get_category_children', to: 'products#get_category_children', defaults: { format: 'json' }
+      get 'get_category_grandchildren', to: 'products#get_category_grandchildren', defaults: { format: 'json' }
+    end
+  end
 end
