77b606a9731c04b8cc72fc51ee111424f06a17f4
yumiki06 <65440153+yumiki06@users.noreply.github.com>
2020-08-25 00:55:18 +0900

    Create grandchild category's HTML

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -16,3 +16,14 @@ $(function(){
     $('.append__category').append(childHtml);
   }
 
+  // 孫カテゴリーの表示作成
+  function appendGrandchidrenBox(insertHTML){
+    var grandchildHtml = '';
+    grandchildHtml = `<div class="category__child" id="grandchildren_wrapper">
+                        <select id="grandchild__category" name="product[category_id]" class="selectbox_style mb_30">
+                          <option value="">---</option>
+                          ${insertHTML}
+                          </select>
+                      </div>`;
+    $('.append__category').append(grandchildHtml);
+  }
