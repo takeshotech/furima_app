ad077382332ab7b81eabb47a3ccbec3a61a0a9ae
yumiki06 <65440153+yumiki06@users.noreply.github.com>
2020-08-25 00:54:55 +0900

    Create child category's HTML

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -4,3 +4,15 @@ $(function(){
     var html = `<option value="${category.id}">${category.name}</option>`;
     return html;
   }
+  // 子カテゴリーの表示作成
+  function appendChidrenBox(insertHTML){
+    var childHtml = '';
+    childHtml = `<div class="category__child" id="children_wrapper">
+                  <select id="child__category" name="product[category_id]" class="selectbox_style mb_30">
+                    <option value="">---</option>
+                    ${insertHTML}
+                  </select>
+                </div>`;
+    $('.append__category').append(childHtml);
+  }
+
