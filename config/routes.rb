029e07ecae3638df57c59e2705ebb7918a9b19c2
yumiki06 <65440153+yumiki06@users.noreply.github.com>
2020-08-25 18:01:13 +0900

    Rewrite routing of products

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   root 'products#index'
-  resources :products, only: [:index, :new, :create, :show]
-  resources :orders, only: :show
-  resources :products do
+  resources :products, only: [:index, :new, :create, :show] do
     collection do
       get 'get_category_children', to: 'products#get_category_children', defaults: { format: 'json' }
       get 'get_category_grandchildren', to: 'products#get_category_grandchildren', defaults: { format: 'json' }
     end
   end
+  resources :orders, only: :show
 end
