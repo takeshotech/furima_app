92502ee363c1a08412a214edee2d1eaa0e4a30d5
yumiki06 <65440153+yumiki06@users.noreply.github.com>
2020-08-25 15:46:22 +0900

    Add category colmun

--- db/migrate/20200808093630_create_products.rb
+++ db/migrate/20200808093630_create_products.rb
@@ -7,6 +7,7 @@ class CreateProducts < ActiveRecord::Migration[5.2]
       t.integer :price, null:false
       t.integer :trading_status, null:false
       t.datetime :completed_at
+      t.references :category, null: false, foreign_key: true
       t.references :shipping, null: false, foreign_key: true
       t.references :brand, foreign_key:true
       t.references :user, null: false, foreign_key: true
