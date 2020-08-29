985ca664bcc6f65781f827405988f92fbf16020b
mayukoala <mayu.3104.j@gmail.com>
2020-08-29 13:01:38 +0900

    Update application_controller.rb
    ベーシック認証のための記述が変わっていた。ここがエラーの原因っぽい気がするので修正。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def basic_auth
-    authenticate_or_request_with_http_basic do |nickname, password|
+    authenticate_or_request_with_http_basic do |username, password|
       username == Rails.application.credentials[:basic_auth][:user] &&
       password == Rails.application.credentials[:basic_auth][:pass]
     end
