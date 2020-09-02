e47e55fa002925364c098022bc7d4bb724d8cf3b
jorry-git <67242361+jorry-git@users.noreply.github.com>
2020-09-02 10:11:06 +0900

    create credit view
    クレジットカード登録・削除・編集のビュー作成並びに登録画面の装飾

---
+++ app/controllers/credit_cards_controller.rb
@@ -0,0 +1,72 @@
+class CreditCardsController < ApplicationController
+  require "payjp" 
+
+  def new
+  end
+
+  def create
+    Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_SECRET_KEY)
+
+    if params["payjp_token"].blank?
+      redirect_to action: "new", alert: "クレジットカードを登録できませんでした。"
+    else
+      customer = Payjp::Customer.create(
+        email: current_user.email,
+        card: params["payjp_token"],
+        metadata: {user_id: current_user.id} 
+      )
+      @card = CreditCard.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
+      if @card.save
+      else
+        redirect_to action: "create"
+      end
+    end
+  end
+
+  def show
+    if @card.blank?
+      redirect_to action: "new" 
+    else
+      Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_SECRET_KEY)
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      @customer_card = customer.cards.retrieve(@card.card_id)
+      @card_brand = @customer_card.brand
+      case @card_brand
+      when "Visa"
+        @card_src = "visa.png"
+      when "JCB"
+        @card_src = "jcb.png"
+      when "MasterCard"
+        @card_src = "master.png"
+      when "American Express"
+        @card_src = "amex.png"
+      when "Diners Club"
+        @card_src = "diners.png"
+      when "Discover"
+        @card_src = "discover.png"
+      end
+
+      @exp_month = @customer_card.exp_month.to_s
+      @exp_year = @customer_card.exp_year.to_s.slice(2,3)
+    end
+  end
+
+  def destroy
+    @card = CreditCard.find_by(user_id: current_user.id)
+    if @card.blank?
+      redirect_to action: "new"
+    else
+      Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_SECRET_KEY)
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      customer.delete
+      @card.delete
+      if @card.destroy
+
+      else
+        redirect_to credit_card_path(current_user.id), alert: "削除できませんでした。"
+      end
+    end
+  end
+
+end
+
