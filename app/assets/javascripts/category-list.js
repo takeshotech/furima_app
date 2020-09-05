e51cd08e8460b691f9eea71f4d6ab8625140267c
yumiki06 <65440153+yumiki06@users.noreply.github.com>
2020-09-05 18:21:53 +0900

    Add mouseover function

--- app/assets/javascripts/category-list.js
+++ app/assets/javascripts/category-list.js
@@ -4,3 +4,10 @@ $(function() {
     var html =`<a class="child_category" id="${child.id}" href="/category/${child.id}">${child.name}</a>`;
     return html;
   }
+
+  $(".parent_category").on("mouseover", function() {
+    var id = this.id //どのリンクにマウスが乗っているかを取得
+    $(".selected-green").removeClass("selected-green") //cssで色を付与する為
+    $('#' + id).addClass("selected-green");
+    $(".child_category").remove();
+    $(".grandchild_category").remove();
