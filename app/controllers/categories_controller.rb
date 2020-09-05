5c34b15aa57bac9569f7af95181d69ef9bfd0bdd
yumiki06 <65440153+yumiki06@users.noreply.github.com>
2020-09-05 18:18:46 +0900

    Add new action

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,10 @@
 class CategoriesController < ApplicationController
+  def new
+    @parents = Category.where(ancestry: nil)
+    @children = Category.find(params[:parent_id]).children
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
 end
