d61f1b5eff32009a5cc4d863942cfff9304946c9
yumiki06 <65440153+yumiki06@users.noreply.github.com>
2020-09-05 18:18:56 +0900

    Add parent action

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,4 +7,21 @@ class CategoriesController < ApplicationController
       format.json
     end
   end
+
+  def parent
+    @category = Category.find(params[:id])
+    children = @category.children
+    grandchildren = []
+    # ancestryに親要素id・子要素idが入っている要素全て取得
+    children.each do |child|
+      grandchildren << Category.where(ancestry: "#{@category.id}/#{child.id}")
+    end
+    @products = []
+    # grandchildrenは多重配列の為、each文を二回重ねることで親要素がもつ全ての孫要素のidをwhereメソッドに渡す
+    grandchildren.each do |grandchild|
+      grandchild.each do |id|
+        @products += Product.where(category_id: id)
+      end
+    end
+  end
 end
