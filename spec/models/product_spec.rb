3bdbdd17d02047bd1b4c96881959edfb24912477
mayukoala <mayu.3104.j@gmail.com>
2020-09-08 00:28:40 +0900

    Update product_spec.rb

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -8,5 +8,32 @@ describe Product, type: :model do
         expect(build(:product, brand_id: nil)).to be_valid
       end
     end
+
+    context 'productを保存できない場合' do
+      it 'nameが空だと保存できないこと' do
+        product = build(:product, name: nil)
+        product.valid?
+        expect(product.errors[:name]).to include("を入力してください")
+      end 
+
+      it 'textが空だと保存できないこと' do
+        product = build(:product, text: nil)
+        product.valid?
+        expect(product.errors[:text]).to include("を入力してください")
+      end
+      
+      it 'priceが空だと保存できないこと' do
+        product = build(:product, price: nil)
+        product.valid?
+        expect(product.errors[:price]).to include("を入力してください")
+      end
+
+      it 'category_idが空だと保存できないこと' do
+        product = build(:product, category_id: nil)
+        product.valid?
+        expect(product.errors[:category]).to include("を入力してください")
+      end
+    end
+
   end
 end
