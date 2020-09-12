04e5b53ac06fed85c290479de5ec61293de28515
mayukoala <mayu.3104.j@gmail.com>
2020-09-12 20:40:04 +0900

    Update routes.rb
    変なスペースが入った影響でカテゴリ機能が動かなくなってしまったので修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :edit, :update, :show]
   root 'home#index'
   resources :products, only: [:new, :create, :show] do
-
-    collection do
+      collection do
       get 'get_category_children', to: 'products#get_category_children', defaults: { format: 'json' }
       get 'get_category_grandchildren', to: 'products#get_category_grandchildren', defaults: { format: 'json' }
     end
