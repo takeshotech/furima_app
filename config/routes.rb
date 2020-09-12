5739c492f2b094069a16a4b2669b68f18e60447b
mayukoala <mayu.3104.j@gmail.com>
2020-09-12 13:06:11 +0900

    Update routes.rb
    デプロイ時のエラーの原因かも？
    indexは必要なさそうなので削除
    
    エラー文
    ActionController::RoutingError (No route matches [GET] "/shell"):

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:index, :edit, :update, :show]
   root 'home#index'
-  resources :products, only: [:index, :new, :create, :show] do
+  resources :products, only: [:new, :create, :show] do
     collection do
       get 'get_category_children', to: 'products#get_category_children', defaults: { format: 'json' }
       get 'get_category_grandchildren', to: 'products#get_category_grandchildren', defaults: { format: 'json' }
