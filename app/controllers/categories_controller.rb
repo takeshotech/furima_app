81c3546c9f576c9e52e324b2734dc8e995d8f4fe
yumiki06 <65440153+yumiki06@users.noreply.github.com>
2020-09-13 22:22:40 +0900

    Delete unnesessary codes

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,44 +1,22 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show]
+
   def index
     @parents = Category.where(ancestry: nil)
   end
 
-  def new
-    @parents = Category.where(ancestry: nil)
-    @children = Category.find(params[:parent_id]).children
-    respond_to do |format|
-      format.html
-      format.json
-    end
+  def show
+    @products = @category.set_products
+    @products = @products.order("created_at DESC").page(params[:page]).per(9)
   end
 
-  def parent
+  private
+  def set_category
     @category = Category.find(params[:id])
-    children = @category.children
-    grandchildren = []
-    # ancestryに親要素id・子要素idが入っている要素全て取得
-    children.each do |child|
-      grandchildren << Category.where(ancestry: "#{@category.id}/#{child.id}")
+    if @category.has_children?
+      @category_links = @category.children
+    else
+      @category_links = @category.siblings
     end
-    @products = []
-    # grandchildrenは多重配列の為、each文を二回重ねることで親要素がもつ全ての孫要素のidをwhereメソッドに渡す
-    grandchildren.each do |grandchild|
-      grandchild.each do |id|
-        @products += Product.where(category_id: id)
-      end
-    end
-  end
-
-  def child
-    grandchildren = @category.children
-    @products = []
-    #子要素の孫要素という一つの配列の為、一度のeach文で孫要素のidを取得
-    grandchildren.each do |grandchild|
-      @products += Product.where(category_id: grandchild.id)
-    end
-  end
-
-  def grandchild
-    @products = Product.where(category_id: params[:id])
   end
 end
