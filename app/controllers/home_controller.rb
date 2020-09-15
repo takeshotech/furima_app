6a7f86aef6b2648868530fe18cfd088087a1e2ab
yumiki06 <65440153+yumiki06@users.noreply.github.com>
2020-09-15 21:17:56 +0900

    Add index action getting data from Category

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   def index
     @images = ProductImage.order("created_at DESC").page(params[:page]).per(5)
     @products = Product.order("created_at DESC").page(params[:page]).per(5)
+    @parents = Category.where(ancestry: nil)
   end
 end
