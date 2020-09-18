75f8fdd9308042b1a564bbaaa4634b6a8041676f
jorry-git <67242361+jorry-git@users.noreply.github.com>
2020-09-18 18:29:24 +0900

    fix
    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,14 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:index, :edit, :update, :show]
   resources :products, only: [:index, :new, :create, :show] do
-    resources :product_images, only: [:new, :create]
-    resources :shippings, only: [:new, :create]
-    resources :brands, only: [:new, :create]
-    collection do
-      get 'get_category_children', to: 'products#get_category_children', defaults: { format: 'json' }
-      get 'get_category_grandchildren', to: 'products#get_category_grandchildren', defaults: { format: 'json' }
+      collection do
+        get 'get_category_children', to: 'products#get_category_children', defaults: { format: 'json' }
+        get 'get_category_grandchildren', to: 'products#get_category_grandchildren', defaults: { format: 'json' }
+
+      resources :orders, only: [:create, :show] 
     end
-    
   end
-  resources :orders 
+  
   resources :credit_cards, only: [:new, :create, :show, :destroy] 
   
 end
